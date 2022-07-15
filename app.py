7dea969d6ae76b32b98819f08622bc0bccc7972b
Anisah Binte Sudarman <108395247+TripleAs-Anisah@users.noreply.github.com>
2022-07-15 11:50:04 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, render_template
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.h5','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    length = float(request.form.get('length'))
+    diameter = float(request.form.get('diameter'))
+    height = float(request.form.get('height'))
+    
+    ##Test model prediction with static data. Reshape to change to 2D array 
+    testdata = np.reshape([
+    0,
+    length,
+    diameter,
+    height,
+    0,
+    0,
+    0,
+    0
+    ],(1, -1))
+
+    pred_result = model.predict(testdata)
+
+    return render_template('index.html', prediction_text='The predicted abalone age is: {:.2f} years.'.format(pred_result[0]))
+
+if __name__ == "__main__":
+    app.run()
